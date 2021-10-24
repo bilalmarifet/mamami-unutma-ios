87a2742a0aba60c95b4207babf03e9419bcbe441
oguz marifet <oguz.marfet@adesso.com.tr>
2021-10-24 17:36:12 +0300

    Initial Commit

---
+++ MamamiUnutma/MamamiUnutmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MamamiUnutmaApp.swift
+//  MamamiUnutma
+//
+//  Created by Oguz Marifet on 24.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MamamiUnutmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
